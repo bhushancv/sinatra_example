a8430ecb98396df52d995a40def0117bfdc9e56d
bhushanjadhavinfo <jadhavbhushan30@gmail.com>
2017-12-18 22:44:38 +0530

    Added bootstrap

--- app.rb
+++ app.rb
@@ -1,6 +1,19 @@
 require 'sinatra'
 
+Levels = {
+  'Level0' => 'No proficiency',
+  'Level1' => 'Elementary proficiency',
+  'Level2' => 'Limited working proficiency',
+  'Level3' => 'Professional working proficiency',
+  'Level4' => 'Full professional proficiency',
+  'Level5' => 'Native or bilingual proficiency'
+}
+
 get '/' do
 	erb :index
 end
 
+post '/proficiency' do
+  @proficiency  = params['proficiency']
+  erb :proficiency
+end
